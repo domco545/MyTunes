d609fd02d8d4f30b0b9060adde3398868b048368
Dominik Rothbauer <dominikrothbauer@gmail.com>
2019-11-25 11:56:51 +0100

    song be

--- src/mytunes/be/Song.java
+++ src/mytunes/be/Song.java
@@ -9,6 +9,11 @@ package mytunes.be;
  *
  * @author domin
  */
-public class deleteME {
-    
+public class Song {
+    private int id;
+    private String title;
+    private String artist;
+    private String category;
+    private int time;
+    private String path;
 }
