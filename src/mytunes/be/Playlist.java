567ec18b0cb5d0c6cb56d3f1f8dc403886d60b54
Dominik Rothbauer <dominikrothbauer@gmail.com>
2019-11-25 12:02:01 +0100

    Create Playlist.java

---
+++ src/mytunes/be/Playlist.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.be;
+
+/**
+ *
+ * @author domin
+ */
+public class Playlist {
+    private int id;
+    private String name;
+    private int song_count;
+    private int playlist_time;
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
