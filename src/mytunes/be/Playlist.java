abf509338619a7c1df8e60dca8fa44c105801296
Martin Brodersen <martinroager@gmail.com>
2019-11-26 11:50:56 +0100

    Update Playlist.java

--- src/mytunes/be/Playlist.java
+++ src/mytunes/be/Playlist.java
@@ -1,8 +1,5 @@
 
 package mytunes.be;
-
-import java.util.*;
-import java.io.*;
 /**
  *
  * @author Martin
@@ -10,27 +7,10 @@ import java.io.*;
 public class Playlist{
     private int id;
     private String name;
-    
-    private int playlist_time;
 
-    public Playlist(String name) {
-      
+    public Playlist(int id, String name) {
+      this.id = id;
+      this.name = name;
 	
-	//Creates a Playlist object that contains an ArrayList for Songs.
-	public Playlist()
-	{
-		playlist = new ArrayList<Song>();
-	}
-	
-	//Adds a Song to a Playlist.
-	public void addSong(Song song)
-	{
-		playlist.add(song);
-	}
-	
-	//Returns the size of a Playlist.
-	public int getPlaylistSize()
-	{
-		return playlist.size();
-	}
     }
+}
