2775b0165614810c199a53f603f01bba1288d3bf
Rocio-hub <56824899+Rocio-hub@users.noreply.github.com>
2019-11-26 10:30:18 +0100

    z

--- src/mytunes/be/Genre.java
+++ src/mytunes/be/Genre.java
@@ -30,42 +30,4 @@ public class Genre {
     public String getName() {
         return name;
     }
-    
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mytunes.be;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- *
- * @author domin
- */
-public class Genre {
-
-    public Genre(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    private int id;
-    private String name;
-
-  
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    
-
-}
-=======
-}
 }
